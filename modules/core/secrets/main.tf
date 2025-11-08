b88102d9ae076307958bc2df2c87619fafc0c36b
barry.pekerman <barrypek@gmail.com>
2025-11-08 16:54:55 +0200

    refactor: remove callback_url from Secrets Manager

--- modules/core/secrets/main.tf
+++ modules/core/secrets/main.tf
@@ -24,10 +24,22 @@ resource "aws_secretsmanager_secret_version" "chatops_secrets" {
     telegram_bot_token = var.telegram_bot_token
     api_gateway_key    = var.api_gateway_key
   })
+}
 
-  lifecycle {
-    ignore_changes = [secret_string]
-  }
+# Project Registry - Always Enabled
+# Stores multiple Terraform projects in a single registry secret
+resource "aws_secretsmanager_secret" "project_registry" {
+  name        = "${var.name_prefix}/project-registry"
+  description = "ChatOps project registry - stores all Terraform project configurations"
+
+  tags = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "project_registry" {
+  secret_id = aws_secretsmanager_secret.project_registry.id
+  secret_string = jsonencode({
+    projects = {}
+  })
 }
 
 
